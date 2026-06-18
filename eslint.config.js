779a275f06d69d2c6cbeef63081609a8f9aaf31b
konstantin <k.hromakovskiy@yandex.ru>
2026-06-18 21:49:46 +0300

    chore: добаивл новое правило в конфигурацию линтера

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,16 @@
 import js from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import stylistic from '@stylistic/eslint-plugin'
+import globals from 'globals'
 
-export default tseslint.config(js.configs.recommended, stylistic.configs.recommended)
+export default tseslint.config(
+  js.configs.recommended,
+  stylistic.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+)
