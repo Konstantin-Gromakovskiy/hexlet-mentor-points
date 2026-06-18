948223c57fdd3cf6a3c8d19fc2aef97d12f3a60e
konstantin <k.hromakovskiy@yandex.ru>
2026-06-18 23:45:29 +0300

    chore: проверка коммита на линтер и донастройка lint-staged

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,5 +3,5 @@ import { integer, pgTable, varchar } from 'drizzle-orm/pg-core'
 export const usersTable = pgTable('users', {
   id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
   first_name: varchar('name', { length: 255 }).notNull(),
-  telegramId: varchar('telegramId', { length: 255 }).notNull(),
+  telegramId: varchar('telegramId', { length: 8 }).notNull(),
 })
