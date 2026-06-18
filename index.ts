c35a7268086ff2f64aaea0e87bbf2899f0706d73
konstantin <k.hromakovskiy@yandex.ru>
2026-06-18 21:19:03 +0300

    style(index): применил линтер для index.js

--- index.ts
+++ index.ts
@@ -1,14 +1,13 @@
-import {Bot} from "grammy";
+import { Bot } from 'grammy'
 
 const API_KEY = process.env.BOT_API_KEY
 
-if (!API_KEY) throw new Error("API_KEY is not defined");
+if (!API_KEY) throw new Error('API_KEY is not defined')
 
-const index = new Bot(API_KEY);
+const index = new Bot(API_KEY)
 
-index.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
+index.command('start', ctx => ctx.reply('Welcome! Up and running.'))
 
-index.on("message", (ctx) => ctx.reply("Привет!21"));
+index.on('message', ctx => ctx.reply('Привет!21'))
 
-index.start();
-
+index.start()
