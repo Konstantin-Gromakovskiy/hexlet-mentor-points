84ea1cd4523b09f2e98fce887bb6936306592446
konstantin <k.hromakovskiy@yandex.ru>
2026-06-22 23:38:06 +0300

    feat: добаивл новые колонки бд, написал первую миграцию

---
+++ drizzle/0000_giant_viper.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "users" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "users_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"first_name" varchar(255) NOT NULL,
+	"telegram_id" varchar(255) NOT NULL,
+	"username" varchar(255),
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_telegram_id_unique" UNIQUE("telegram_id")
+);
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,10 @@
-import { integer, pgTable, varchar } from 'drizzle-orm/pg-core'
+import { integer, pgTable, varchar, timestamp } from 'drizzle-orm/pg-core'
 
 export const usersTable = pgTable('users', {
   id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
-  first_name: varchar('name', { length: 255 }).notNull(),
-  telegram_id: varchar('telegramId', { length: 255 }).notNull(),
-  username: varchar('username', { length: 255 }).notNull(),
-  created_at: varchar('createdAt', { length: 255 }).notNull(),
-  updated_at: varchar('updatedAt', { length: 255 }).notNull(),
+  firstName: varchar('first_name', { length: 255 }).notNull(),
+  telegramId: varchar('telegram_id', { length: 255 }).notNull().unique(),
+  username: varchar('username', { length: 255 }),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
 })
