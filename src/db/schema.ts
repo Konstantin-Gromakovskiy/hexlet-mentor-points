8584738314c2a42b061bb6d9f6d42936e4960002
konstantin <k.hromakovskiy@yandex.ru>
2026-06-23 10:57:11 +0300

    feat(lint): добавил роль для схемы бд, сгенерировал миграцию

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,13 @@
-import { integer, pgTable, varchar, timestamp } from 'drizzle-orm/pg-core'
+import { integer, pgTable, varchar, timestamp, pgEnum } from 'drizzle-orm/pg-core'
+
+const userRoleEnum = pgEnum('role', ['tutor', 'admin'])
 
 export const usersTable = pgTable('users', {
   id: integer('id').primaryKey().generatedAlwaysAsIdentity(),
   firstName: varchar('first_name', { length: 255 }).notNull(),
   telegramId: varchar('telegram_id', { length: 255 }).notNull().unique(),
   username: varchar('username', { length: 255 }),
+  role: userRoleEnum().notNull().default('tutor'),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at').notNull().defaultNow(),
 })
